c77d15417827fa589c4fd7f708d986bb174ad5c4
Aditya Upadhyay <adityaupadhyay3004@gmail.com>
2018-03-27 18:32:08 -0400

    Railroad.py changed

--- scrapers/railroad_crossings.py
+++ scrapers/railroad_crossings.py
@@ -7,8 +7,9 @@ Created on Fri Mar 16 15:45:40 2018
 
 import pandas as pd
 import zipfile
+import mysqlConnection as md
 
-def main():
+def importing_railroad(file_name):
     
     # low_memory=False : fixes warnings about mixed types because dropping those columns in next step anyways
 #    railroad_locations = pd.read_csv("resources/Railroad_Crossings.csv", low_memory=False) 
@@ -16,8 +17,9 @@ def main():
 #    print(railroad_locations.shape)
     
     zf = zipfile.ZipFile('resources/Railroad_Crossings.zip') 
-    railroad_locations = pd.read_csv(zf.open('Railroad_Crossings.csv'), sep="\t", low_memory=False,  encoding='latin-1')
+    railroad_locations = pd.read_csv(zf.open(file_name), sep="\t", low_memory=False,  encoding='latin-1')
     railroad_locations = railroad_locations[['RAILROAD', 'StateCode','LATITUDE', 'LONGITUD']]
     #print(railroad_locations)
-    
-main()
+    md.create_table(md.connect(), railroad_locations, 'railroad_crossings')
+   
+importing_railroad('Railroad_Crossings.csv')
