e6560c54f187779586388d782702c396f75c9d87
ecmikovi <ecmikovi@ncsu.edu>
2018-04-03 16:55:03 -0400

    added state to weather scraper

--- scrapers/weatherscraper.py
+++ scrapers/weatherscraper.py
@@ -10,7 +10,7 @@ import mysqlConnection as md
 
 def import_weather(file_name):
     df_weather = pd.read_csv("resources/" + file_name, low_memory=False)
-    df_weather = df_weather[['StationName', 'Date', 'ObsType', 'Value', 'S-Flag', 'City']]  
+    df_weather = df_weather[['StationName', 'Date', 'ObsType', 'Value', 'S-Flag', 'City', 'State']]  
     
     md.create_table(md.connect(), df_weather, 'weather_observations')
 
