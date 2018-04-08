88d09a189962f8724ccba137664b39515e6f8208
doneria-anjali <anjali.doneria@gmail.com>
2018-04-08 15:41:16 -0400

    State code mapping

---
+++ scrapers/statecodemapping.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import mysqlConnection as md
+
+def read_state_codes(file_name):
+    state_code = pd.read_csv('resources/' + file_name, sep = '|')
+    print(state_code.head(5))
+    md.create_table(md.connect(), state_code, 'state_codes')
+    
+    
+read_state_codes('statecodemapping.txt')
