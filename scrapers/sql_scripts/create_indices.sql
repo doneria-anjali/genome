de1b6a56cc8bdfd015aaab061b849c971338f1a1
doneria-anjali <anjali.doneria@gmail.com>
2018-04-08 23:15:18 -0400

    Final indices

--- scrapers/sql_scripts/create_indices.sql
+++ scrapers/sql_scripts/create_indices.sql
@@ -8,9 +8,3 @@ create index `or_state` on dddm.oil_reserve(state(50));
 
 create index `w_state` on dddm.weather_observations(State(50));
 create index `w_city` on dddm.weather_observations(City(50));
-
-#indices on final tables
-create index `orf_zip` on dddm.oil_reserve_final(zip(50));
-create index `lpf_zip` on dddm.land_prices_final(zip(50));
-create index `spf_zip` on dddm.seaports_final(Zipcode(50));
-create index `spf_zip` on dddm.weather_final(zip(50));
