f254a1bf93cd5295a4f327606ff0f52403687efa
Cameron Estroff <crestrof@ncsu.edu>
2018-04-12 18:27:23 -0400

    Updated model program

--- scrapers/buildModel.py
+++ scrapers/buildModel.py
@@ -10,6 +10,12 @@ import pandas as pd
 import numpy as np
 import mysqlConnection as md
 
+model_names = []
+train_accuracies = []
+test_accuracies = []
+cv_scores = []
+class_names = ['yes', 'no']
+
 def plot_confusion_matrix(cm, classes,
                           normalize=False,
                           cmap=plt.cm.Blues):
@@ -41,13 +47,62 @@ def plot_confusion_matrix(cm, classes,
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
 
-def build_Gaussian_model():
+def plot_outcome_chart(predicted_values, title='Predicted outcomes'):
+    objects = ('No', 'Yes')
+    y_pos = np.arange(len(objects))
+    values = list(predicted_values.values())
+    if len(values) < 3:
+        values.insert(0, 0)
+    plt.bar(y_pos, values, align='center', alpha=0.5)
+    plt.xticks(y_pos, objects)
+    plt.ylabel('Count')
+    plt.title(title)
+    plt.show()
     
-    model_names = []
-    train_accuracies = []
-    test_accuracies = []
-    cv_scores = []
-    class_names = ['yes', 'no']
+def plot_model_comparison(model_names, train_accuracies, test_accuracies, cv_scores):
+    raw_data = pd.DataFrame(
+        {'model': model_names,
+         'train_accuracy': train_accuracies,
+         'test_accuracy': test_accuracies,
+         'cv_score': cv_scores
+        })
+    pos = list(range(len(raw_data['train_accuracy']))) 
+    width = 0.25 
+    fig, ax = plt.subplots(figsize=(20,10))
+    
+    plt.bar(pos, 
+            raw_data['train_accuracy'], 
+            width, 
+            alpha=0.5, 
+            color='#B3CCFF', 
+            label=raw_data['model'][0]) 
+    
+    plt.bar([p + width for p in pos], 
+            raw_data['test_accuracy'],
+            width, 
+            alpha=0.5, 
+            color='#4D88FF', 
+            label=raw_data['model'][1]) 
+    
+    plt.bar([p + width*2 for p in pos], 
+            raw_data['cv_score'],
+            width, 
+            alpha=0.5, 
+            color='#003399', 
+            label=raw_data['model'][2])
+    ax.set_ylabel('Accuracy')
+    ax.set_title('Model Comparison')
+    ax.set_xticks([p + 1.5 * width for p in pos])
+    ax.set_xticklabels(raw_data['model'])
+    plt.xlim(min(pos)-width, max(pos)+width*4)
+    plt.ylim( [0, 1] )
+    # Adding the legend and showing the plot
+    plt.legend(['Training Accuracy', 'Testing Accuracy', 'CV Score'], loc='upper left')
+    plt.grid()
+    plt.show()
+
+def build_Gaussian_model():
+    class_names = ['No', 'Yes']
     
     datafull = pd.read_sql_table('model_data', md.connect())
     data = datafull[['seaport', 'landprice', 'oilreserve', 'existingplants', 'disasters', 'railroad', 'populationdensity']]
@@ -63,8 +118,8 @@ def build_Gaussian_model():
     
     GNB_scores = cross_val_score(GNB_clf, data, target.values.ravel(), cv=cv)
     
-    #unique, counts = np.unique(GNB_predicted, return_counts=True)
-    #GNB_counts = dict(zip(unique, counts))
+    unique, counts = np.unique(GNB_predicted, return_counts=True)
+    GNB_counts = dict(zip(unique, counts))
     
     GNB_confusion = confusion_matrix(target,GNB_predicted)
     model_names.append('GaussianNB')
@@ -84,43 +139,43 @@ def build_Gaussian_model():
     print("  Cross validation score: %0.2f (+/- %0.2f)" % (GNB_scores.mean(), GNB_scores.std() * 2))
     print("  Predicted values accuracy: %0.2f" % (cv_score))
     
-    #plot_outcome_chart(GNB_counts)
-    #plt.figure()
+#    plot_outcome_chart(GNB_counts)
+    plt.figure()
     plot_confusion_matrix(GNB_confusion, class_names)
     plt.show()
     print(classification_report(target, GNB_predicted)) 
     print()
     
 def build_AdaBoost_model():
-    class_names = ['yes', 'no']
+    class_names = ['No', 'Yes']
     
     datafull = pd.read_sql_table('model_data', md.connect())
-    data = datafull[['seaport', 'landprice', 'oilreserve', 'existingplants', 'disasters', 'railroad', 'populationdensity']]
+    data = datafull[['landprice', 'existingplants', 'disasters', 'railroad', 'populationdensity']]
     target = datafull[['actual']]
     
     x_train, x_test, y_train, y_test = train_test_split(data, target, test_size=0.2, random_state=3)
     cv = ShuffleSplit(n_splits=5, test_size=0.2, random_state=3)
     
-    AB_clf = AdaBoostClassifier(n_estimators = 200, random_state = 3)
+    AB_clf = AdaBoostClassifier(n_estimators = 500, random_state = 3)
     AB_clf.fit(x_train, y_train['actual'])
     
     AB_predicted = cross_val_predict(AB_clf, data, target['actual'])
-    AB_scores = cross_val_score(AB_clf, data, target, cv=cv)
+    AB_scores = cross_val_score(AB_clf, data, target.values.ravel(), cv=cv)
     
-    #unique, counts = np.unique(AB_predicted, return_counts=True)
-    #AB_counts = dict(zip(unique, counts))
+    unique, counts = np.unique(AB_predicted, return_counts=True)
+    AB_counts = dict(zip(unique, counts))
     
     AB_confusion = confusion_matrix(target,AB_predicted)
-    #model_name = 'AdaBoost'
+    model_names.append('AdaBoost')
     
     train_accuracy = accuracy_score(y_train, AB_clf.predict(x_train))
-    #train_accuracies.append(train_accuracy)
+    train_accuracies.append(train_accuracy)
     
     test_accuracy = accuracy_score(y_test, AB_clf.predict(x_test))
-    #test_accuracies.append(test_accuracy)
+    test_accuracies.append(test_accuracy)
     
     cv_score = accuracy_score(target, AB_predicted)
-    #cv_scores.append(cv_score)
+    cv_scores.append(cv_score)
     
     print("AdaBoost Classifier Model: ")
     print("  Score of {} for training set: {:.4f}.".format(AB_clf.__class__.__name__, train_accuracy))
@@ -128,8 +183,8 @@ def build_AdaBoost_model():
     print("  Cross validation score: %0.2f (+/- %0.2f)" % (AB_scores.mean(), AB_scores.std() * 2))
     print("  Predicted values accuracy: %0.2f" % (accuracy_score(target, AB_predicted) ))
     
-    #plot_outcome_chart(AB_counts)
-    #plt.figure()
+#    plot_outcome_chart(AB_counts)
+    plt.figure()
     plot_confusion_matrix(AB_confusion, class_names)
     plt.show()
     print(classification_report(target, AB_predicted)) 
@@ -138,7 +193,12 @@ def build_AdaBoost_model():
     importances = AB_clf.feature_importances_
     indices = np.argsort(importances)[::-1]
     print ('Feature Ranking: ')
-    for i in range(0,6):
+    for i in range(0,4):
         print ("{} feature no.{} ({})".format(i+1,indices[i],importances[indices[i]]))
-        
-build_AdaBoost_model()
+
+def runAllModels():
+    build_Gaussian_model()
+    build_AdaBoost_model()
+#    plot_model_comparison(model_names, train_accuracies, test_accuracies, cv_scores)
+    
+#runAllModels()
