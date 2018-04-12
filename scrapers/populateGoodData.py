fa8e8193f6643f1d6e6a48b867b22d5e9738920b
doneria-anjali <anjali.doneria@gmail.com>
2018-04-12 14:40:53 -0400

    updated code

--- scrapers/populateGoodData.py
+++ scrapers/populateGoodData.py
@@ -5,14 +5,13 @@ import buildModelAttributes as model
 
 def populateData(radius, actualValue):
     #select zipcodes from table
-    query = "select distinct(zip_code) FROM dddm.plant_locations limit 0,50"
+    query = "select distinct(zip_code) FROM dddm.plant_locations limit 0,40"
     data = pd.read_sql(query, md.connect())
     
     zipList = data['zip_code'].tolist()
-    
+        
     for zipcode in zipList:
-        model.addToTable(zipcode, radius, actualValue)
-        print("Processed " + zipcode)
-        break
+        model.addToTable(str(int(zipcode)), radius, actualValue)
+        print("Processed " + str(int(zipcode)))
     
 populateData(50, 'Y')
